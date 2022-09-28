9e282822d072f571d19f1c66bf303a5d1a0dba81
Cauan Sousa <103227067+cauansousa@users.noreply.github.com>
2022-09-28 19:26:05 -0300

    Add files via upload

--- hera_face/src/lib/mainlib (copy).py
+++ hera_face/src/lib/mainlib (copy).py
@@ -1,5 +1,4 @@
 from fnmatch import fnmatch
-from re import A, I
 import dlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -45,7 +44,7 @@ print(known_name)
 # Load the image in real time, have to change the path to de video capture
 #small_frame = cv2.imread("/home/robofei/catkin_hera/src/3rdParty/vision_system/hera_face/src/lib/face_images/teste.jpg")
 
-
+face_name = []
 #open camera   
 video_capture = cv2.VideoCapture(0)
 while video_capture.isOpened():
@@ -56,36 +55,35 @@ while video_capture.isOpened():
         img_shape = sp(small_frame, img_detected[0])
         align_img = dlib.get_face_chip(small_frame, img_shape)
         img_rep = np.array(model.compute_face_descriptor(align_img))
-        
         # Compare the face in real time with the faces that we already have
         #aux = 1
         #compare each face in the image with the faces in the database
         for i in range(0, len(img_rep)):
             name = "Face"
-            face_name = []
+            
             for j in range(0, len(known_face)):
                 euclidean_dist = list(np.linalg.norm(known_face - img_rep, axis=1) <= 0.6)
                 if True in euclidean_dist:
                     fst = euclidean_dist.index(True)
                     name = known_name[fst]
-                    print("Face detected: " + name)
                     break
-
-            face_name.append(name)
+            if name not in face_name:
+                face_name.append(name)
+        print(face_name)
         #print(face_name)
         #bounding box
-        for face, name in zip(img_detected, face_name):
-            x1 = face.left()
-            y1 = face.top()
-            x2 = face.right()
-            y2 = face.bottom()
-
-            cv2.rectangle(small_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(small_frame, name, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        for i, rects in enumerate(img_detected):
+            
+            cv2.rectangle(small_frame, (rects.left(), rects.top()), (rects.right(), rects.bottom()), (0, 255, 0), 2)
+            print(rects.left(), rects.top(), rects.right(), rects.bottom())
+            for name in face_name:    
+                print(name)     
+                cv2.putText(small_frame, name, (rects.left(), rects.top()), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
         
-    except CvBridgeError as e:
-        print(e)
+    except: #CvBridgeError as e:
+        #print(e)
+        pass
     # Show the image
     cv2.imshow('Video', small_frame)
 
--- hera_face/src/lib/mainlib.py
+++ hera_face/src/lib/mainlib.py
@@ -1,13 +1,13 @@
 from fnmatch import fnmatch
-from re import A
 import dlib
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2
 import os
 from pyexpat import model
 import fnmatch
 import cv2
+from cv_bridge import CvBridgeError
+
 
 def euclidean(x, y):
     return np.sqrt(np.sum((x - y) ** 2))
@@ -39,8 +39,10 @@ for f in range(0, len(files)):
         else:
             print("No face detected in image: " + files[f])
             break
+
+
 # Load the image in real time, have to change the path to de video capture
-small_frame = cv2.imread("/home/robofei/catkin_hera/src/3rdParty/vision_system/hera_face/src/lib/face_images/teste.jpg")
+small_frame = cv2.imread("/home/robofei/catkin_hera/src/3rdParty/vision_system/hera_face/src/lib/baggio.jpg")
 
 # Detect the faces in the image
 img_detected = detector(small_frame, 1)
@@ -48,34 +50,36 @@ img_shape = sp(small_frame, img_detected[0])
 align_img = dlib.get_face_chip(small_frame, img_shape)
 img_rep = np.array(model.compute_face_descriptor(align_img))
 
+face_name = []
 # Compare the face in real time with the faces that we already have
-aux = 1
-for i in range(0, len(known_face)):
-    #euclidian distance
-    euclidean_dist = euclidean(known_face[i], img_rep)
-
-    if euclidean_dist < 0.6 and euclidean_dist < aux:
-        aux = euclidean_dist
-        face = known_face[i]
-        name = known_name[i]
-        print("Face detected: " + known_name[i])
-
+for i in range(0, len(img_rep)):
+    name = "Face"
+            
+    for j in range(0, len(known_face)):
+        euclidean_dist = list(np.linalg.norm(known_face - img_rep, axis=1) <= 0.6)
+        if True in euclidean_dist:
+            fst = euclidean_dist.index(True)
+            name = known_name[fst]
+            print("Face detected: " + name)
+            break
+    if name not in face_name:
+        face_name.append(name)
+print(face_name)
+#print(face_name)
 #bounding box
-for face in img_detected:
-    x1 = face.left()
-    y1 = face.top()
-    x2 = face.right()
-    y2 = face.bottom()
-    cv2.rectangle(small_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    cv2.putText(small_frame, name, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+for i, rects in enumerate(img_detected):
+    for name in face_name:    
+        print(name)     
+        cv2.rectangle(small_frame, (rects.left(), rects.top()), (rects.right(), rects.bottom()), (0, 255, 0), 2)
+        cv2.putText(small_frame, name, (rects.left(), rects.top()), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
     # Show the image
-    cv2.imshow('Video', small_frame)
+cv2.imshow('Video', small_frame)
 
-    cv2.waitKey(0)
+cv2.waitKey(0)
     # Press q to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
+if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
         
     #close all open windows
 
