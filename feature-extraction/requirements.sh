a97c4d470a8d1b4e9c7328e742fb5edaa168f321
Cauan Sousa <103227067+cauansousa@users.noreply.github.com>
2022-09-29 21:47:28 -0300

    Update requirements.sh

--- feature-extraction/requirements.sh
+++ feature-extraction/requirements.sh
@@ -9,20 +9,19 @@ pip3 install colorgram.py
 pip3 install extcolors 
 pip3 install webcolors
 sudo apt-get install imagemagick
-tensorflow>=2.3.0
-tensorflow-estimator>=2.3.0
-keras>=2.1.0
-keras-Applications>=1.0.8
-keras-Preprocessing>=1.1.2
-imutils>=0.5.3
-numpy>=1.18.2
-opencv-python>=4.2.0.*
-opencv-python-headless>=4.5.1.48
-matplotlib>=3.3.4
-argparse>=1.1
-scipy>=1.4.1
-scikit-learn>=0.23.1
-pillow>=7.2.0
-streamlit>=0.76.0
-omegaconf>=2.0.6
-h5py>=2.10.0
+sudo apt-get install tensorflow>=2.3.0
+sudo apt-get install tensorflow-estimator>=2.3.0
+sudo apt-get install keras>=2.1.0
+sudo apt-get install keras-Applications>=1.0.8
+sudo apt-get install keras-Preprocessing>=1.1.2
+sudo apt-get install imutils>=0.5.3
+sudo apt-get install numpy>=1.18.2
+sudo apt-get install opencv-python>=4.2.0.*
+sudo apt-get install opencv-python-headless>=4.5.1.48
+sudo apt-get install matplotlib>=3.3.4
+sudo apt-get install scipy>=1.4.1
+sudo apt-get install scikit-learn>=0.23.1
+sudo apt-get install pillow>=7.2.0
+sudo apt-get install streamlit>=0.76.0
+sudo apt-get install omegaconf>=2.0.6
+sudo apt-get install h5py>=2.10.0
